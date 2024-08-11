b987f5e03fda72b13a3d413798724ad365a64657
Suke2004 <lyricsofsongs96@gmail.com>
2024-08-11 23:46:44 +0530

    media

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icons from './components/Icons';
+
+function App() {
+  return (
+    <div className="App">
+      <Icons />
+    </div>
+  );
+}
+
+export default App;
